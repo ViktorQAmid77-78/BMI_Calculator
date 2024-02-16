363777aab761cbeb0b33ceb1efadd299050c1aea
Viktor Alekseevich <com.brick.ozon@gmail.com>
2024-02-16 17:42:30 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(int weightKg, double heightM) {
+        double bmi = weightKg / (heightM * heightM);
+        return (int) bmi;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98; // вес в килограммах
+        double height = 1.87; // рост в метрах
+
+        int result = service.calculate(weight, height);
+        System.out.println("BMI: " + result);
+    }
+}
